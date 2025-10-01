584b2d25101fe6e36310687dd5cc45fc1f99d79f
Chaitanya Keshniya <chaitanyakeshniya1404@gmail.com>
2025-10-01 22:29:50 +0530

    add: declare DOM constants

---
+++ script.js
@@ -0,0 +1,11 @@
+const genrateBtn = document.getElementById('generate-btn');
+const passwordOne = document.getElementById('password-one');
+const passwordTwo = document.getElementById('password-two');
+const lengthSlider = document.getElementById('length');
+const lengthVal1 = document.getElementById('length-value');
+const includeNumbers = document.getElementById('include-number');
+const includeSymbols = document.getElementById('include-symbols');
+const copyBtns = document.querySelectorAll('.copy-btn');
+const strengthText = document.getElementById('strength-text');
+const strengthBars = document.querySelectorAll('.strength-bar');
+
