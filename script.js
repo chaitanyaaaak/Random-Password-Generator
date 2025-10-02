ecbd2f07d537d808c4835279c27392e6150fc9f3
Chaitanya Keshniya <chaitanyakeshniya1404@gmail.com>
2025-10-02 16:06:10 +0530

    feat: add password generation functionality

--- script.js
+++ script.js
@@ -18,3 +18,36 @@ const CHARSETS = {
     SYMBOLS: '~`!@#$%^&*()_-+={[}]|:;"<,>.?/',
 }
 
+// Events 
+
+lengthSlider.addEventListener('input', (e) => {
+    lengthVal1.textContent = e.target.value;
+});
+
+genrateBtn.addEventListener('click', renderPasswords);
+
+copyBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const targetId = e.currentTarget.dataset.target;
+        copyToClipboard(targetId, e.currentTarget)
+    });
+});
+
+
+// password generate function
+
+function generatePassword() {
+    let charset = CHARSETS.LOWERCASE + CHARSETS.UPPERCASE;
+    if (options.numbers) charset += CHARSETS.NUMBERS;
+    if (options.symbols) charset += CHARSETS.SYMBOLS;
+
+    // if (charset.length === 0) return '';
+
+    let password = '';
+    for (let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    return password;
+}; 
